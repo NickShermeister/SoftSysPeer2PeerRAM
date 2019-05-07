9dacbdeb5742eab97cb4beb9fc4470db0bf30b71
CleanestMink126 <gns126@gmail.com>
2019-05-07 02:31:30 -0400

    updated donate

--- code/donate.c
+++ code/donate.c
@@ -26,24 +26,16 @@ void print_message(char *message) {
   puts("\n");
 }
 
-
-int main(void) {
-  //We need a hashmap to find stuf given an ID
-  hashmap* hm = declare_map(hashCode);
-  int skip_for_data= 6;
-  //various helpful variables
+void get_IP(struct sockaddr_in* serv_addr){
   int sockfd=0, p=0, n=0, i, PORT;
-  char receive_buffer[recv_buffer_size];
-  struct sockaddr_in serv_addr;
-  char * send_buffer = malloc(send_buffer_size * (sizeof(char)));
-
   char IP_ADDR[22];
   char str_port[6];
   char c;
-  //Get the IP address from the user
+
+  //Parse the IP
   puts("Please Enter IP Address of Server:");
   fgets(IP_ADDR, 21, stdin);
-  //Parse the IP address and port
+
   for(i=0; i<strlen(IP_ADDR); i++) {
     if(p) {
       str_port[p-1] = IP_ADDR[i];
@@ -55,24 +47,83 @@ int main(void) {
       p = 1;
     }
   }
+
   IP_ADDR[n] = '\0';
+
   str_port[p-1] = '\0';
   PORT = strtol(str_port, '\0', 10);
-  //Clear our buffer
-  memset(receive_buffer, '\0' ,sizeof(receive_buffer));
 
-  serv_addr.sin_family = AF_INET;
-  serv_addr.sin_port = htons(PORT);
-  serv_addr.sin_addr.s_addr = inet_addr(IP_ADDR);
+  serv_addr->sin_family = AF_INET;
+  serv_addr->sin_port = htons(PORT);
+  serv_addr->sin_addr.s_addr = inet_addr(IP_ADDR);
+}
+
+void become_donor(struct sockaddr_in serv_addr){
+  //Add self as donor
+  char send_buffer[send_buffer_size];
+  // char receive_buffer[recv_buffer_size];
+  puts("Server Message: ");
+  print_message(receive_buffer);
+  close(sockfd);
 
   //Add self as donor
   //Declare socket
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
   //Inform server that you exist
   connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
   //Inform the server that you are a donor
   strcpy(send_buffer, "3");
   write(sockfd, send_buffer, strlen(send_buffer));
+}
+
+store(hashmap* hm, char* recv_buffer){
+  int skip_for_data= 6;
+  //Accepting data
+  //Get the id number first
+  int ID = atoi(receive_buffer+2*(sizeof(char)));
+  //Get where the actual information starts
+  char* data_start = receive_buffer + skip_for_data * sizeof(char);
+  int string_size = strlen(data_start);
+  //Save a copy so we can service other requests
+  char* data_copy = malloc(sizeof(char) * (string_size+1));
+  strcpy(data_copy, data_start);
+  //Put copy in hashmap under the key ID
+  insert(hm, ID, (void*) data_copy);
+  // printf("Save:\nID:%u\n", ID);
+  write(sockfd, "Did the thing", 14);
+
+  // printf("%s\n", data_copy);
+}
+
+retrieve(hashmap* hm, char* recv_buffer){
+  //Sending data back
+  //Again parse ID
+  int ID = atoi(receive_buffer+2*(sizeof(char)));
+  // printf("Load:\nID: %u", ID);
+  DataItem* d = search(hm, ID);
+  if(d==NULL){
+    printf("Yikes");
+    send_buffer[0] = '\0';
+    write(sockfd, send_buffer, 1);
+    continue;
+  }
+  //Get the pointer
+  char* stored_data = (char*)d->data;
+  write(sockfd, stored_data, strlen(stored_data));
+}
+
+int main(void) {
+  //We need a hashmap to find stuf given an ID
+  hashmap* hm = declare_map(hashCode);
+  //various helpful variables
+  int sockfd=0, p=0, n=0, i, PORT;
+  char receive_buffer[recv_buffer_size];
+  struct sockaddr_in serv_addr;
+  char * send_buffer = malloc(send_buffer_size * (sizeof(char)));
+  get_IP(&serv_addr);
+  become_donor(serv_addr);
+  //Clear our buffer
+  memset(receive_buffer, '\0' ,sizeof(receive_buffer));
   //Actually accept requests
   while(1){
     //Clear memory from last request
@@ -82,44 +133,15 @@ int main(void) {
     }else{
       //We successfully filled the buffer
       // printf("Server port: %d\n", sockfd);
-      unsigned int ID;
-      char* stored_data;
       //Parse the mode
       int mode = ((int)receive_buffer[0]) - 48;
       // printf("Mode: %d\n",mode);
       switch(mode){
         case 1:
-          //Accepting data
-          //Get the id number first
-          ID = atoi(receive_buffer+2*(sizeof(char)));
-          //Get where the actual information starts
-          char* data_start = receive_buffer + skip_for_data * sizeof(char);
-          int string_size = strlen(data_start);
-          //Save a copy so we can service other requests
-          char* data_copy = malloc(sizeof(char) * (string_size+1));
-          strcpy(data_copy, data_start);
-          //Put copy in hashmap under the key ID
-          insert(hm, ID, (void*) data_copy);
-          // printf("Save:\nID:%u\n", ID);
-          write(sockfd, "Did the thing", 14);
-
-          // printf("%s\n", data_copy);
+          store(hm, receive_buffer);
           break;
         case 2:
-          //Sending data back
-          //Again parse ID
-          ID = atoi(receive_buffer+2*(sizeof(char)));
-          // printf("Load:\nID: %u", ID);
-          DataItem* d = search(hm, ID);
-          if(d==NULL){
-            printf("Yikes");
-            send_buffer[0] = '\0';
-            write(sockfd, send_buffer, 1);
-            continue;
-          }
-          //Get the pointer
-          stored_data = (char*)d->data;
-          write(sockfd, stored_data, strlen(stored_data));
+          retrieve(hm, receive_buffer);
           break;
         case 3:
           //for testing purposes. Forget what we know
